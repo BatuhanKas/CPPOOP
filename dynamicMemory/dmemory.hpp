62b7bed1961d6788e3287f7c34e07f87252bb059
Batuhan Kas <bkas@k2m03s08.42kocaeli.com.tr>
2024-05-28 11:52:45 +0300

    update

--- dynamicMemory/dmemory.hpp
+++ dynamicMemory/dmemory.hpp
@@ -6,7 +6,7 @@
 /*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 17:50:32 by bkas              #+#    #+#             */
-/*   Updated: 2024/05/27 18:16:31 by bkas             ###   ########.fr       */
+/*   Updated: 2024/05/28 11:46:15 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@ using namespace std;
 class Example {
    public:
     int x, y;
-    Example() { cout << "Const Worked" << endl; };
+    Example(int mx = 0, int my = 0);
     ~Example() { cout << "Dest Worked" << endl; };
     void display() { cout << "x val: " << x << " Y val: " << y << endl; };
+    Example addSum(Example &oth);
 };
 
 #endif
---
+++ dynamicMemory/thisp.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thisp.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 11:37:03 by bkas              #+#    #+#             */
+/*   Updated: 2024/05/28 11:52:38 by bkas             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "dmemory.hpp"
+
+/**
+ * @brief This bizim nesnemizin adresini tutuyor,
+ * Degeri diyerek nesnemizi return ediyoruz.
+ * Kendi nesnemi return ediyoruz cagirdigim nesneyi degil.
+ *
+ * @param oth
+ * @return Example
+ */
+
+Example Example::addSum(Example &oth) {
+    this->x += oth.x;
+    this->y += oth.y;
+    return *this;
+}
+
+Example::Example(int mx, int my) {
+    this->x = mx;
+    this->y = my;
+    this->display();
+    cout << "Const Worked" << endl;
+}
+
+int main() {
+    Example e1(5, 5), e2(4, 4);
+
+    Example e3 = e1.addSum(e2);
+    e3.display();
+}
