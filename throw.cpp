7fb466d565311560694bf4a0ff1c2b0f2d082b9f
Batuhan Kas <bkas@k2m03s08.42kocaeli.com.tr>
2024-07-19 14:24:25 +0300

    update

---
+++ throw.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   throw.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 12:54:40 by bkas              #+#    #+#             */
+/*   Updated: 2024/07/19 14:24:05 by bkas             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+void islem(int x, int y) throw(logic_error, string) {
+    if (x == 0) {
+        throw logic_error("Bir sayi sifir olamaz");
+    }
+    if (x < 0 || y < 0) {
+        string errmsg = "bir sayi neg olamaz.";
+        throw errmsg;
+    }
+    cout << "Result: " << x / y << endl;
+}
+
+int main() {
+    try {
+        int x = 12, y = -3;
+
+        islem(x, y);
+    } catch (logic_error &le) {
+        cout << "logic err: " << le.what() << endl;
+    } catch (runtime_error &re) {
+        cout << "runtime err: " << re.what() << endl;
+    } catch (string errmsg) {
+        cout << "hata :>" << errmsg << endl;
+    }
+}
--- throwCatch/main.cpp
+++ throwCatch/main.cpp
@@ -6,14 +6,14 @@
 /*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 18:05:49 by bkas              #+#    #+#             */
-/*   Updated: 2024/07/15 18:20:08 by bkas             ###   ########.fr       */
+/*   Updated: 2024/07/19 14:18:41 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 using namespace std;
 
-void checknum(int num) {
+void checknum(int num) throw(runtime_error, logic_error) {
     if (num < 0) {
         throw runtime_error("num can't be lower than zero");
     } else if (num == 0) {
@@ -25,7 +25,7 @@ void checknum(int num) {
 
 int main() {
     try {
-        checknum(31);
+        // checknum(31);
         checknum(0);
         // checknum(-1);
     } catch (const runtime_error &e) {
